486d1964c4df3351c42bbfbeb6e04ec8d01cfc20
edoardope <pencoedoardo@gmail.com>
2024-07-14 20:02:51 +0200

    fix

--- app/Http/Controllers/Api/cmdController.php
+++ app/Http/Controllers/Api/cmdController.php
@@ -13,39 +13,39 @@ class cmdController extends Controller
 {
     public function createCommand($request){
         // Validazione dei dati ricevuti
-    $validatedData = $request->validate([
-        'istance_key' => 'required|string',
-        'cmd_name' => 'required|string',
-        'ticket' => 'nullable|integer',
-        'side' => 'nullable|integer',
-        'magnum' => 'nullable|integer',
-        'Automatism_id' => 'nullable|integer',
-        'lot' => 'nullable|numeric',
-        'tp' => 'nullable|numeric',
-        'sl' => 'nullable|numeric',
-        'comment' => 'nullable|string',
-    ]);
+        $validatedData = $request->validate([
+            'istance_key' => 'required|string',
+            'cmd_name' => 'required|string',
+            'ticket' => 'nullable|integer',
+            'side' => 'nullable|integer',
+            'magnum' => 'nullable|integer',
+            'Automatism_id' => 'nullable|integer',
+            'lot' => 'nullable|numeric',
+            'tp' => 'nullable|numeric',
+            'sl' => 'nullable|numeric',
+            'comment' => 'nullable|string',
+        ]);
+    
+        // Creazione del nuovo record nella tabella command_queues
+        DB::table('command_queues')->insert([
+            'istance_key' => $validatedData['license_key'],
+            'cmd_name' => $validatedData['cmdname'],
+            'ticket' => $validatedData['ticket'],
+            'side' => $validatedData['side'],
+            'magnum' => $validatedData['magnum'],
+            'Automatism_id' => $validatedData['Automatism_id'],
+            'lot' => $validatedData['lot'],
+            'tp' => $validatedData['tp'],
+            'sl' => $validatedData['sl'],
+            'comment' => $validatedData['comment'],
+            'created_at' => Carbon::now('Europe/Rome'),
+            'updated_at' => Carbon::now('Europe/Rome'),
+        ]);
+    
+        // Puoi restituire una risposta o eseguire ulteriori azioni qui
+        return response()->json([
+            'succes' => true
+        ]);
 
-    // Creazione del nuovo record nella tabella command_queues
-    DB::table('command_queues')->insert([
-        'istance_key' => $validatedData['license_key'],
-        'cmd_name' => $validatedData['cmdname'],
-        'ticket' => $validatedData['ticket'],
-        'side' => $validatedData['side'],
-        'magnum' => $validatedData['magnum'],
-        'Automatism_id' => $validatedData['Automatism_id'],
-        'lot' => $validatedData['lot'],
-        'tp' => $validatedData['tp'],
-        'sl' => $validatedData['sl'],
-        'comment' => $validatedData['comment'],
-        'created_at' => Carbon::now('Europe/Rome'),
-        'updated_at' => Carbon::now('Europe/Rome'),
-    ]);
-
-    // Puoi restituire una risposta o eseguire ulteriori azioni qui
-    return response()->json([
-        'succes' => true
-    ]);
-
-}
+    }
 }
